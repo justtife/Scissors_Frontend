c443e9e60cb8c67a34acd80940b8db6229c17c77
Justtife <farinubolu@gmail.com>
2023-07-19 23:12:03 +0200

    Updated the frontend meta tag

--- src/app/components/share/share.component.ts
+++ src/app/components/share/share.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 @Component({
   selector: 'app-share',
   templateUrl: './share.component.html',
   styleUrls: ['./share.component.scss'],
 })
 export class ShareComponent {
-  constructor() {}
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
   shareLink() {
+    const imageUrl =
+      'https://resizing.app/blog/how-to-resize-image-without-losing-quality/irfanview.jpg';
+    const text = 'Check out this image!';
+    const shareData = {
+      title: 'Your application title',
+      text,
+      url: 'https://jumia.com/shoes',
+      files: [new File([imageUrl], 'image.jpg', { type: 'image/jpeg' })],
+    };
     if (navigator.share) {
       navigator
-        .share({
-          title: 'Your application title',
-          text: 'Check out this link!',
-          url: 'https://jumia.com/shoes',
-        })
+        .share(shareData)
         .then(() => console.log('Shared successfully'))
         .catch((error) => console.error('Error sharing:', error));
     } else {
